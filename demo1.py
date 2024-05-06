12af0708c3cf286f3a7b1434de1c64503deb938e
Faizel Ahmed <feisala132@gmail.com>
2024-05-06 15:46:30 +0100

    Adding text surface

--- demo1.py
+++ demo1.py
@@ -10,9 +10,12 @@ screen = pygame.display.set_mode((screen_WIDTH, screen_HEIGHT))
 screen.fill('Black')
 pygame.display.set_caption("demo")
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = test_font.render('Demo game', False, 'Black')
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -21,6 +24,7 @@ while True:
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 60))
     # draw all our elements
     # update everything
     pygame.display.update()
