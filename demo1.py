93e77e0ac734a1a62cd042deea74ae89da6eaaec
Faizel Ahmed <feisala132@gmail.com>
2024-05-06 15:32:12 +0100

    Adding sky and ground surface

--- demo1.py
+++ demo1.py
@@ -7,19 +7,20 @@ screen_HEIGHT = 400
 
 
 screen = pygame.display.set_mode((screen_WIDTH, screen_HEIGHT))
-screen.fill('White')
+screen.fill('Black')
 pygame.display.set_caption("demo")
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Blue')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    screen.blit(test_surface, (200, 100))
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
     # draw all our elements
     # update everything
     pygame.display.update()
