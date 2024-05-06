8415ae56fc71c0328bb202a5162cf70e9d7f4b4d
Faizel Ahmed <feisala132@gmail.com>
2024-05-06 18:15:31 +0100

    Adding snail turtle

--- demo1.py
+++ demo1.py
@@ -15,8 +15,12 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
 text_surface = test_font.render('Demo game', False, 'Black')
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
 
-while True:
+game_running = True
+
+while game_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -25,8 +29,19 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 60))
+    snail_x_pos -= 4
+
+    if snail_x_pos < 0:
+        snail_x_pos = 800
+
+
+    screen.blit(snail_surface, (snail_x_pos, 250))
+
+
     # draw all our elements
     # update everything
     pygame.display.update()
     clock.tick(60)
+
+
 
